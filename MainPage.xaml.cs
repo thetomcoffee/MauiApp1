166880d5af2e78bf0e25f00e00ef098c3107235b
thetomecoffee <tom@tomswebsite.org>
2022-08-27 15:50:30 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		DateTime current = DateTime.Now;
+
+		DateTimeLabel.Text = current.ToLongDateString() + "\r\n" + current.ToLongTimeString();
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+

a4ca71b55d063fc4dac456d0a9673bcceeff9931
thetomecoffee <tom@tomswebsite.org>
2022-08-27 16:56:05 -0500

    Removed OnCounterClicked().

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,23 +9,7 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		//Previous code
-		//count++;
-
-		//if (count == 1)
-		//	CounterBtn.Text = $"Clicked {count} time";
-		//else
-		//	CounterBtn.Text = $"Clicked {count} times";
-
-		//DateTime current = DateTime.Now;
-
-		//DateTimeLabel.Text = current.ToLongDateString() + "\r\n" + current.ToLongTimeString();
-
-  //      SemanticScreenReader.Announce(CounterBtn.Text);
 
 
-	}
 }
 

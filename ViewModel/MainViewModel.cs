d91c1a0a9e8f27ee12f6d0529892722102cd3eac
thetomecoffee <tom@tomswebsite.org>
2022-09-12 19:00:52 -0500

    Added ViewModel folder, MainViewModel.cs.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.ComponentModel;
+
+namespace MauiApp1.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name) => 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
